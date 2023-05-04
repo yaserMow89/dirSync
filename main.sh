8ec75aecf23dda4bd31239a53bc9e72035db2034
yaserMow89 <yaserMow89@gmail.com>
2023-05-04 17:51:00 +0200

    nothing

--- main.sh
+++ main.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
-echo "Please enter the directory location that you want to sync: "
-read LOCAL_DIR_LOCATION
 
-echo "$LOCAL_DIR_LOCATION"
+# -----------------------------------------------------------------------
+# Function definitions - start
+
+
+funcDirValidity () {
+if [ -d "$1" ]; then
+    VALIDITY_CHECK="PASSED"
+    echo "$STR_DIR_ACK"
+else 
+    VALIDITY_CHECK="FAILED"
+fi
+}
+
+
+funcListDir () {
+    ls -R "$1"
+}
+
+# -----------------------------------------------------------------------
+# Function definitions - stop
+
+
+STR_DIR_ACK="The directory exists" 
+STR_ASK_DIR_LOCATION="Please enter the directory location that you want to sync: "
+
+echo "$STR_ASK_DIR_LOCATION"
+read LOCAL_DIR_LOCATION
+funcDirValidity "$LOCAL_DIR_LOCATION"
+
+
+while [ "$VALIDITY_CHECK" != "PASSED" ]; do
+    echo "The directory does not exist"
+    echo "$STR_ASK_DIR_LOCATION"
+    read LOCAL_DIR_LOCATION
+    funcDirValidity "$LOCAL_DIR_LOCATION"
+    
+done
+
+
+funcListDir "$LOCAL_DIR_LOCATION"
 
 
